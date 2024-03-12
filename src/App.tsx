a48d6d14d41b037cc3c9b4b1d6446090057bd693
Filippo Giampapa <fgiampapa@2program.it>
2024-03-12 16:39:18 +0100

    loading UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,36 @@ import {
   setToZero,
 } from "./features/counter/counter-slices";
 import { useFetchBreedsQuery } from "./features/cats/cats-api-slice";
-import { Suspense } from "react";
+
+
 
 function App() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
 
   const { data, isFetching } = useFetchBreedsQuery(10);
+  const Loading = () => {
+    return <section className="grid grid-cols-4 gap-2 p-2">
+      {Array.from({ length: 10 }).map((_, index) => {
+      return <div key={index} className="animate-pulse bg-gray-300 h-[250px] w-full rounded">
+    
+      </div>;
+    }
+    )}
+    </section>
+   
+  }
 
   console.log(data)
   return (
     <>
       <div>
-       {isFetching ? <p>Loading...</p> :  <p>{data?.length} cats breeds fetched</p>}
-       <section className="grid grid-cols-4 gap-2">
+       {isFetching ? <Loading /> :  <p>{data?.length} cats breeds fetched</p>}
+       <section className="grid grid-cols-4 gap-2 p-2">
        {data && data.map((breed) => {
         return <div className="relative" key={breed.id}>
           <p className="absolute bottom-0 p-2 bg-gradient-to-r from-white/50 to-transparent">{breed.name}</p>
-          <Suspense fallback={<p>Loading...</p>}>
             <img className="w-full h-full object-cover" src={breed.image.url} alt={breed.name} />
-          </Suspense>
         </div>
        })}
        </section>
